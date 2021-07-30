b838d32e0d81d2a42a15148ca322b131a8b2c09f
rStelmach <robert.stelmach7@wp.pl>
2021-07-30 18:58:19 +0200

    feat: add header and nav styling

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,23 +1,25 @@
-// import cart from '../../assets/cart.png';
+// // import cart from '../../assets/cart.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
-    <header className="flex justify-between  bg-red-500 p-8">
-      <h1 className="px-56 font-bold text-white text-4xl tracking-wide text-shadow-md">
+    <header className="flex  align-baseline text-white justify-between  bg-red-500 p-8">
+      <h1 className="px-56 font-bold  text-4xl tracking-wide text-shadow-md">
         EasyOrder
       </h1>
       <nav>
-        <ul className="flex justify-between w-64 font-semibold text-white text-2xl text-shadow-sm ">
+        <ul className="flex justify-between w-64 font-semibold  text-2xl text-shadow-sm ">
           <li className="cursor-pointer">Menu</li>
           <li className="cursor-pointer">GitHub</li>
         </ul>
       </nav>
-      {/* <img src={cart} alt="cart" className="" /> */}
+
       <button
         type="button"
-        className=" mr-36 font-semibold text-white text-2xl"
+        className=" mr-36 font-semibold  text-2xl ml-2"
       >
-        Cart
+        <FontAwesomeIcon icon={faShoppingCart} /> Cart
       </button>
     </header>
   );
